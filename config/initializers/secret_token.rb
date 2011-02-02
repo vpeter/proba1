9e6f90b5516cb38cd7be922e5334f8ff81e8fd5b
vpeter <vagnerpeter01@gmail.com>
2011-02-02 23:53:57 +0100

    alap projekt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proba1::Application.config.secret_token = '3836f0d7d9602590725d023b82516a2937b9c768202b5611432859b59d01d0e57f6522e19e4047044375f0ba0a1ca3010f4b23361a6ff4ed9d490fa7e3be99c3'
