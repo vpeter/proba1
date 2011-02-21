3f7b1aea95d82daa96a551f76a8709b64f672d70
vpeter <vagnerpeter01@gmail.com>
2011-02-21 23:54:40 +0100

    title kiiras ketfele modon + CSS

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,27 @@
 class PagesController < ApplicationController
   def home
     $aktualis = "home"
+    @title = "hát üdvözöljük névjegykártya rendelő oldalunkon!"
   end
 
   def order_description
     $aktualis = "order_description"
+    @title = "így rendelhet"
   end
 
   def contact
     $aktualis = "contact"
+    @title = "kapcsolat velünk"
   end
 
   def policy
     $aktualis = "policy"
+    @title = "szabályzatunk"
   end
 
   def faq
     $aktualis = "faq"
+    @title = "kérdések és válaszok"
   end
 
 end
