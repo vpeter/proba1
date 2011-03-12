42425a1076f8161835e5c948011fcf87f64a52fd
vpeter <vagnerpeter01@gmail.com>
2011-03-12 01:31:47 +0100

    megrendeles menete, footer es aprobb dolgok

--- app/models/order.rb
+++ app/models/order.rb
@@ -29,20 +29,20 @@
 class Order < ActiveRecord::Base
   ORDERTYPES = ("A".."I").to_a
   PRINTTYPES = ("1".."5").to_a
-  validates :order_type, :presence => true, :inclusion => { :in => ORDERTYPES }
-  validates :print, :presence => true, :inclusion => { :in => PRINTTYPES }
-  validates :quantity, :presence => true, :numericality => {:greater_than => 0, :only_integer => true}
-#  validates :corner
-  validates :print_ready, :presence => true
-  validates :contact_name, :presence => true, :length => { :maximum => 30 }
-  validates :contact_phone, :presence => true
-  validates :contact_mail, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
-#  validates :contact_mail, :format => { :with => /\A([-a-z0-9!\#$%&'*+\/=?^_`{|}~]+\.)*[-a-z0-9!\#$%&'*+\/=?^_`{|}~]+@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
-  validates :contact_note, :presence => true
-  validates :billing_name, :presence => true, :length => { :maximum => 30 }
-  validates :billing_address, :presence => true
-  validates :delivery_note, :presence => true
-  validates :takeover, :presence => true
-  validates :payment, :presence => true
-  validates :order_note, :presence => true
+#  validates :order_type, :presence => true, :inclusion => { :in => ORDERTYPES }
+#  validates :print, :presence => true, :inclusion => { :in => PRINTTYPES }
+#  validates :quantity, :presence => true, :numericality => {:greater_than => 0, :only_integer => true}
+##  validates :corner
+#  validates :print_ready, :presence => true
+#  validates :contact_name, :presence => true, :length => { :maximum => 30 }
+#  validates :contact_phone, :presence => true
+#  validates :contact_mail, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+##  validates :contact_mail, :format => { :with => /\A([-a-z0-9!\#$%&'*+\/=?^_`{|}~]+\.)*[-a-z0-9!\#$%&'*+\/=?^_`{|}~]+@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+#  validates :contact_note, :presence => true
+#  validates :billing_name, :presence => true, :length => { :maximum => 30 }
+#  validates :billing_address, :presence => true
+#  validates :delivery_note, :presence => true
+#  validates :takeover, :presence => true
+#  validates :payment, :presence => true
+#  validates :order_note, :presence => true
 end
