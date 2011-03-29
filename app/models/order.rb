a9bb1a691547603eb5c9edf241b931f8ee2e46c4
vpeter <vagnerpeter01@gmail.com>
2011-03-30 00:18:50 +0200

    paperclip alkalmazsa elotti allapot

--- app/models/order.rb
+++ app/models/order.rb
@@ -29,23 +29,24 @@
 class Order < ActiveRecord::Base
   ORDERTYPES = ("A".."I").to_a
   PRINTTYPES = ("A".."E").to_a
-#  validates :order_type, :presence => true, :inclusion => { :in => ORDERTYPES }
-#  validates :print, :presence => true, :inclusion => { :in => PRINTTYPES }
-#  validates :quantity, :presence => true, :numericality => {:greater_than => 0, :only_integer => true}
-##  validates :corner
-#  validates :print_ready, :presence => true
-#  validates :contact_name, :presence => true, :length => { :maximum => 30 }
-#  validates :contact_phone, :presence => true
-#  validates :contact_mail, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
-##  validates :contact_mail, :format => { :with => /\A([-a-z0-9!\#$%&'*+\/=?^_`{|}~]+\.)*[-a-z0-9!\#$%&'*+\/=?^_`{|}~]+@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
-#  validates :contact_note, :presence => true
-#  validates :billing_name, :presence => true, :length => { :maximum => 30 }
-#  validates :billing_address, :presence => true
-#  validates :delivery_note, :presence => true
-#  validates :takeover, :presence => true
-#  validates :payment, :presence => true
-#  validates :order_note, :presence => true
-#  validates :vkey, :presence => true, :length => { :minimum => 4 }
+  validates :order_type, :presence => true, :inclusion => { :in => ORDERTYPES }
+  validates :print, :presence => true, :inclusion => { :in => PRINTTYPES }
+  validates :quantity, :presence => true, :numericality => {:greater_than => 0, :only_integer => true}
+#  validates :corner
+#  validates :print_ready, :inclusion => {:in => [true, false]}
+  validates :print_ready, :presence => true
+  validates :contact_name, :presence => true, :length => { :maximum => 30 }
+  validates :contact_phone, :presence => true
+  validates :contact_mail, :presence => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+#  validates :contact_mail, :format => { :with => /\A([-a-z0-9!\#$%&'*+\/=?^_`{|}~]+\.)*[-a-z0-9!\#$%&'*+\/=?^_`{|}~]+@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+  validates :contact_note, :presence => true
+  validates :billing_name, :presence => true, :length => { :maximum => 30 }
+  validates :billing_address, :presence => true
+  validates :delivery_note, :presence => true
+  validates :takeover, :presence => true
+  validates :payment, :presence => true
+  validates :order_note, :presence => true
+  validates :vkey, :presence => true, :length => { :minimum => 4 }
   
   before_validation :ensure_vkey_has_a_value
 
